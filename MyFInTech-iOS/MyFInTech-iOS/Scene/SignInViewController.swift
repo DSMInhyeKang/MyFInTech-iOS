71d663c1fc7138a9282adf92f618540a76dd6964
InhyeKang <kanginhae0826@gmail.com>
2024-04-11 23:18:39 +0900

    feat :: Image, Font 추가 및 LanchScreen 퍼블리싱
    근.데? RIBs랑 다른 모듈들 왜 오류....?

--- MyFInTech-iOS/MyFInTech-iOS/Scene/SignInViewController.swift
+++ MyFInTech-iOS/MyFInTech-iOS/Scene/SignInViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SignInViewController.swift
 //  MyFInTech-iOS
 //
 //  Created by 강인혜 on 4/2/24.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SignInViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
